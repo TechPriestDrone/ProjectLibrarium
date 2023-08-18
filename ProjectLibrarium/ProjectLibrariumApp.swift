868b497343c2e591bde5cff52d2d98abaa4e3da4
Andrey <fordeving@icloud.com>
2023-08-18 16:07:53 +0300

    Initial Commit

---
+++ ProjectLibrarium/ProjectLibrariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectLibrariumApp.swift
+//  ProjectLibrarium
+//
+//  Created by ios5 on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectLibrariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
