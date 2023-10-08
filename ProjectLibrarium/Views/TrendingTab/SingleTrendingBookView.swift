87454b75ee119b4892a9bbf014872bad30bbada2
Andrey <fordeving@icloud.com>
2023-10-08 19:16:23 +0300

    changed the trending page to display covers instead of the shelf. the shelf view was commented out.

---
+++ ProjectLibrarium/Views/TrendingTab/SingleTrendingBookView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleTrendingBookView.swift
+//  FoxBooksBox
+//
+//  Created by Andrey Safronov on 08/10/2023.
+//
+
+import SwiftUI
+
+struct SingleTrendingBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleTrendingBookView()
+}
